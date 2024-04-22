27d259ec6a3af0ebf274c95e45b7e8296686d616
Евгений Корзун <89513152520@yandex.ru>
2024-04-22 12:03:08 +0300

    третье задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,25 @@
 function compareArrays(arr1, arr2) {
-  
+
+	if (arr1.length !== arr2.length) {
+		return false
+	}
+
+	return arr1.every((value, index) => {
+		console.log(value, arr2[index]);
+		return value === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	const filtredUsers = users.filter(users => users.gender === gender);
+
+	if (filtredUsers.length === 0) {
+		return 0;
+	}
+
+	const totalAge = filtredUsers.reduce((acc, users) => acc + users.age, 0);
+
+	const averageAge = totalAge / filtredUsers.length;
+	return averageAge
 }
