5c29b8a44362dbafad8544b390d24b1ef63f8cdf
Евгений Корзун <89513152520@yandex.ru>
2024-05-02 11:26:32 +0300

    Восьмое задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,45 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(JSON.stringify(args));
+		let objectInCache = cache.find(item => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша: " + objectInCache.value, cache);
+			return "Из кеша: " + objectInCache.value;
+		}
+		let result = func(...args);
+		cache.push({
+			hash: hash,
+			value: result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result, cache);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
-//Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId;
+	let hasCalled = false;
+
+	function wrapper(...args) {
+		if (!hasCalled) {
+			func(...args);
+			wrapper.count++;
+			hasCalled = true;
+		}
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => {
+			func(args);
+			wrapper.count++;
+		}, delay);
+		wrapper.allCount++;
+	}
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+	return wrapper;
 }
